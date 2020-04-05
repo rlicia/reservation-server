785248bf076fbc18b60c4c36af87b31bfa67e442
rlicia <thanakit.limakun@hotmail.com>
2020-04-05 18:54:50 +0700

    edit

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -6,6 +6,7 @@ const requireAuth = require('../middlewares/requireAuth');
 const User = mongoose.model('User');
 const ClientDetail = mongoose.model('ClientDetail');
 const UserDetail = mongoose.model('UserDetail');
+const UserTier = mongoose.model('UserTier');
 
 const router = express.Router();
 
@@ -16,11 +17,15 @@ router.get('/account',async (req, res) => {
     const { _id, username, status } = req.user;
     try {
         let detail;
+        let permissions;
         if (status === 1) {
             detail = await ClientDetail.findOne({ client: _id }).populate('tier');
+            permissions = null;
         }
         if (status === 0) {
             detail = await UserDetail.findOne({ user: _id }).populate('tier');
+            tier = await UserTier.findById(detail.tier._id);
+            permissions = tier.permissions;
         }
 
         const account = {
@@ -31,7 +36,8 @@ router.get('/account',async (req, res) => {
             email: detail.detail.email,
             gender: detail.detail.gender === 1 ? 'Male' : 'Female',
             tier: detail.tier.tierName.charAt(0).toUpperCase() + detail.tier.tierName.substring(1),
-            accountStatus: detail.status
+            accountStatus: detail.status,
+            permissions: permissions
         };
         res.status(200).send({ account });
     } catch (err) {
